dee5be57a60e5d7c9a4f846fd8bbad0941b7fdee
Darkness <616487222@qq.com>
2018-12-10 10:32:50 +0800

    add: multigroup

---
+++ react-sortable-hoc/index.js
@@ -0,0 +1,10 @@
+export SortableContainer from './SortableContainer';
+export SortableElement from './SortableElement';
+export SortableHandle from './SortableHandle';
+export SortableGroup from './SortableGroup';
+
+export sortableContainer from './SortableContainer';
+export sortableElement from './SortableElement';
+export sortableHandle from './SortableHandle';
+
+export {arrayMove} from './utils';
